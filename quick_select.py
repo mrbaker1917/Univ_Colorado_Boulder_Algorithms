5bd834e08453c188145be51b1de05cb3a903a7b9
Mark R. Baker <mrbaker1917@gmail.com>
2024-03-29 17:16:01 -0700

    update to quick_select.py

--- quick_select.py
+++ quick_select.py
@@ -1,8 +1,9 @@
-'''Found the k-th smallest element in an array using quick select algorithm'''
+"""Found the k-th smallest element in an array using quick select algorithm"""
 
 import random
 import numpy as np
 import time
+
 
 def quick_select(a, k):
     if len(a) == 1:
@@ -16,11 +17,15 @@ def quick_select(a, k):
     elif k <= len(less_than_pivot) + len(equal_to_pivot):
         return pivot
     else:
-        return quick_select(more_than_pivot, k - len(less_than_pivot) - len(equal_to_pivot))
+        return quick_select(
+            more_than_pivot, k - len(less_than_pivot) - len(equal_to_pivot)
+        )
 
-n = 10**8
-k = n//2
-a = [random.randint(2, n) for _ in range(n)]
+
+# n = 10**8
+k = 6
+# a = [random.randint(2, n) for _ in range(n)]
+a = [-7, 3, 5, -4, 6, 8, 12, 9, 15, 7]
 start = time.perf_counter()
 print(quick_select(a, k))
 print(time.perf_counter() - start)
